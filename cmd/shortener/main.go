5b7cc4293c61cc753f488bc89658c9cd396eb7a8
Dmitri Bauer <mail@dmitribauer.com>
2022-07-11 23:02:11 +0300

    Increment 1

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,15 @@
 package main
 
-func main() {}
+import (
+	"github.com/dmitribauer/go-url-shortener/internal/app/api"
+	"github.com/dmitribauer/go-url-shortener/internal/app/urlrep"
+)
+
+func main() {
+	urlRepository := urlrep.NewInMemory(nil)
+	rest := api.NewRest(urlRepository)
+	err := rest.Run("localhost", 8080)
+	if err != nil {
+		panic(err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/dmitribauer/go-url-shortener
+
+go 1.18
+
+require github.com/google/uuid v1.3.0 // indirect
